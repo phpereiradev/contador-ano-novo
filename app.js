11e8d9d4c545575e95523cdd9389a4f77a080a1c
phpereiradev <phellipereiracc02@hotmail.com>
2023-02-27 14:07:20 -0300

    Update do Projeto Contador Ano Novo

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const secondsContanier = document.querySelector('#seconds')
 const minutesContanier = document.querySelector('#minutes')
 const hoursContanier = document.querySelector('#hours')
 const daysContanier = document.querySelector('#days')
+const nextYearContanier = document.querySelector('#year')
+const spinnerLoading = document.querySelector('#loading')
+const countdownContanier = document.querySelector('#countdown')
 
 const nextYear = new Date().getFullYear() + 1
 const newYearTime = new Date(`January 01 ${nextYear} 00:00:00 `)
+nextYearContanier.textContent = nextYear
+
+const getTimeUnit = unit => unit < 10 ? '0' + unit : unit
+
+const insertCountdownValues = ({ days, hours, minutes, seconds }) => {
+    secondsContanier.textContent = getTimeUnit(seconds) 
+    minutesContanier.textContent = getTimeUnit(minutes) 
+    hoursContanier.textContent = getTimeUnit(hours) 
+    daysContanier.textContent = getTimeUnit(days) 
+}
+
 
 const updateCountdown = () => {
     const currentTime = new Date()
@@ -13,11 +27,20 @@ const updateCountdown = () => {
     const hours = Math.floor(difference / 1000 / 60 / 60 ) % 24
     const minutes = Math.floor(difference / 1000 / 60 ) % 60
     const seconds = Math.floor(difference / 1000 ) % 60
+    
+    insertCountdownValues({ days, hours, minutes, seconds })
 
-    secondsContanier.textContent = seconds < 10 ? '0' + seconds : seconds
-    minutesContanier.textContent = minutes < 10 ? '0' + minutes : minutes
-    hoursContanier.textContent = hours < 10 ? '0' + hours : hours
-    daysContanier.textContent = days < 10 ? '0' + days : days
+    
 }
+
+const handleCountdownDisplay  = () => {
+    spinnerLoading.remove()
+    countdownContanier.style.display = 'flex'
+
+}
+
+setTimeout(handleCountdownDisplay, 1000)
+
+
 
 setInterval(updateCountdown, 1000)
